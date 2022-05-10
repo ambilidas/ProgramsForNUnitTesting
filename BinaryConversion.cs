f8390390a1f3668f63046be0b471d21749457b79
Ambili das v s <ambili.kan@gmail.com>
2022-05-10 16:38:43 +0530

    Program for converting decimal value in to binary value is added

---
+++ BinaryConversion.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramsForNUnitTesting
+{
+    internal class BinaryConversion
+    {
+        public void ToBinary()
+        {
+            int i;
+            int[] a = new int[10];
+            Console.WriteLine("\nEnter the number to convert");
+            int n=Convert.ToInt32(Console.ReadLine());
+            for(i=0; n>0; i++)
+            {
+                a[i] = n % 2;
+                n = n / 2;
+                
+            }
+            Console.WriteLine("Binary of the given number is : ");
+            for (i = i - 1; i >= 0; i--)
+            {
+                Console.Write(a[i]);
+            }
+        }
+    }
+}
